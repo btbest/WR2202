2a702bfe93477fe72b82ba164db0213c9c1644f0
Jan Caron <jan.caron@tngtech.com>
2022-03-11 14:43:41 +0100

    Bounding box for left player

--- src/systems/movement_systems.rs
+++ src/systems/movement_systems.rs
@@ -1,9 +1,23 @@
 use bevy::prelude::*;
 
+struct Bounds {
+    x0: f32,
+    x1: f32,
+    y0: f32,
+    y1: f32
+}
 
-
-pub fn move_system_l(input: Res<Input<KeyCode>>, mut query: Query<&mut Transform, With<PlayerL>>) {
+pub fn move_system_l(input: Res<Input<KeyCode>>, mut query: Query<&mut Transform, With<PlayerL>>, windows: Res<Windows>) {
     let speed = 10.;
+    let window = windows.get_primary().unwrap();
+    let height = window.height();
+    let width = window.width();  
+    let bounds = Bounds {
+        x0: -window.width()/2.,
+        x1: -window.width()/6.,
+        y0: -window.height()/2.,
+        y1: window.height()/2.,
+    };
     if input.pressed(KeyCode::W) {
         query.single_mut().translation.y += speed;
     }
@@ -16,6 +30,19 @@ pub fn move_system_l(input: Res<Input<KeyCode>>, mut query: Query<&mut Transform
     if input.pressed(KeyCode::D) {
         query.single_mut().translation.x += speed;
     }
+    if query.single_mut().translation.x < bounds.x0 {
+        query.single_mut().translation.x = bounds.x0;
+    }
+    else if query.single_mut().translation.x > bounds.x1 {
+        query.single_mut().translation.x = bounds.x1;
+    }
+    if query.single_mut().translation.y < bounds.y0 {
+        query.single_mut().translation.y = bounds.y0;
+    }
+    else if query.single_mut().translation.y > bounds.y1 {
+        query.single_mut().translation.y = bounds.y1;
+    }
+
 }
 
 
